1a34efd4c5aea204cd11d3d4bb69ae6a16d14aab
yi_cao <yi_cao@kingdee.com>
2018-01-09 17:52:28 +0800

    youqu project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="haha">123</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'yes'
+      }
+    },
+    methods: {
+      haha() {
+        this.$http.get('/api/a.txt')
+          .then(res => {
+            console.log(res);
+
+          })
+          .catch(e => {
+            console.log(e);
+          })
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'yes'
+    }
+  },
+  mounted() {
+    this.$http.get('http://localhost:8080/#/a.txt').then(res => {
+      console.log(res);
+      
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
